4471600228bf3899d0d78e2019de0c44aac98d86
KeyZenD <37895600+KeyZenD@users.noreply.github.com>
2020-06-06 02:20:00 +0800

    Add files via upload

---
+++ fl.py
@@ -0,0 +1,25 @@
+from asyncio import sleep
+from userbot.events import register
+
+@register(outgoing=True, pattern='^.fl ?(.*)')
+async def fakeload(e):
+	inp = e.pattern_match.group(1)
+	load = [" ","▏","▎","▍","▌","▋","▊","▉"]
+	bar = ""
+	count = 0
+	await e.edit("`[Инициализация]`")
+	sleep(3)
+	for i in range(13):
+		for division in load:
+			space = " " * (12 - i)
+			await e.edit(f"`{bar}{division}{space}[{count}%]`")
+			count += 1
+			sleep(0.3)
+			if count == 101:
+				break
+		bar += "█"
+	sleep(2)
+	done = "Загрузка завершена!"
+	if inp:
+		done = inp
+	await e.edit(f"`{done}`")
