b826a24a2a64ae3f0f59eff14193301c8d63918e
KeyZenD <37895600+KeyZenD@users.noreply.github.com>
2020-09-25 11:39:31 +0800

    Update MacAshoT.py

--- MacAshoT.py
+++ MacAshoT.py
@@ -1,6 +1,6 @@
 from ..  import loader,utils
 from PIL import Image,ImageFilter
-import io
+import io,random,string
 class aMod(loader.Module):
 	strings={'name':'MacAshoT'}
 	async def maccmd(S,message):
@@ -19,10 +19,11 @@ class aMod(loader.Module):
 		else:await A.edit(J);return
 		try:L=Image.open(B).convert(M)
 		except:await A.edit(J);return
-		F,G=L.size;B=Image.new(M,(F,G));B.paste(L,(0,0),L);H=min(F//100,G//100);D=Image.new(M,(F+H*40,G+H*40),'#fff');E=Image.new(M,(F,G))
+		F,G=L.size;B=Image.new(M,(F,G));B.paste(L,(0,0),L);I=min(F//100,G//100);D=Image.new(M,(F+I*40,G+I*40),'#fff');E=Image.new(M,(F,G))
 		for N in range(F):
 			for O in range(G):
 				if B.getpixel((N,O))!=(0,0,0,0):E.putpixel((N,O),(0,0,0))
-		E=E.resize((F+H*5,G+H+5));D.paste(E,((D.width-E.width)//2,(D.height-E.height)//2),E);D=D.filter(ImageFilter.GaussianBlur(H*5));D.paste(B,((D.width-B.width)//2,(D.height-B.height)//2),B);I=io.BytesIO();I.name='macos.png';D.save(I,'PNG');I.seek(0)
-		if K:await C.reply(file=I);await A.delete()
-		else:await A.edit(file=I,text='')
+		E=E.resize((F+I*5,G+I+5));D.paste(E,((D.width-E.width)//2,(D.height-E.height)//2),E);D=D.filter(ImageFilter.GaussianBlur(I*5));D.paste(B,((D.width-B.width)//2,(D.height-B.height)//2),B);H=io.BytesIO();H.name='-'.join([''.join([random.choice(string.hexdigits)for B in range(A)])for A in[5,4,3,2,1]])+'.png';D.save(H,'PNG');H.seek(0)
+		if utils.get_args_raw(A):await A.client.send_file(A.to_id,H,force_document=R);await A.delete()
+		elif K:await C.reply(file=H);await A.delete()
+		else:await A.edit(file=H,text='')
