d9d859ea0ed7f66bb23a6a06d1efa4c8bce9b846
KeyZenD <37895600+KeyZenD@users.noreply.github.com>
2021-11-11 08:17:03 +0800

    Update SuperDemotivator.py

--- SuperDemotivator.py
+++ SuperDemotivator.py
@@ -46,8 +46,9 @@ async def cmds(message, type):
 	bytes_image = await event.download_media(bytes)
 	demotivator = await demotion(font_bytes, bytes_image, text, type)
 	if is_reply:
-		return await event.reply(file=demotivator)
+		a = await event.reply(file=demotivator)
 		await message.delete()
+		return a
 	else:
 		return await event.edit(file=demotivator, text="")
 	
