2dac534c1a667349cae3bab1876a649af2693507
KeyZenD <37895600+KeyZenD@users.noreply.github.com>
2022-01-08 12:25:14 +0800

    Update delme.py

--- delme.py
+++ delme.py
@@ -3,11 +3,6 @@ from .. import loader, utils
 class DelmeMod(loader.Module):
     """Удаляет все сообщения"""
     strings = {'name': 'DelMe'}
-    def __init__(self):
-        self.name = self.strings['name']
-    async def client_ready(self, client, db):
-        message.client = client
-    
     @loader.sudo
     async def delmecmd(self, message):
         """Удаляет все сообщения от тебя"""
@@ -17,7 +12,7 @@ class DelmeMod(loader.Module):
             if args != str(message.chat.id+message.sender_id):
                 await message.edit(f"<b>Если ты точно хочешь это сделать, то напиши:</b>\n<code>.delme {message.chat.id+message.sender_id}</code>")
                 return
-            await delete(message, chat)
+            await delete(chat, message, True)
         else:
             await message.edit("<b>В лс не чищу!</b>")
     @loader.sudo
@@ -25,19 +20,19 @@ class DelmeMod(loader.Module):
         """Удаляет все сообщения от тебя без вопросов"""
         chat = message.chat
         if chat:
-            await delete(message, chat)
+            await delete(chat, message, False)
         else:
             await message.edit("<b>В лс не чищу!</b>")
-            
 
-async def delete(message, chat):
-    all = (await message.client.get_messages(chat, from_user="me")).total
-    messages = [msg async for msg in message.client.iter_messages(chat, from_user="me")]
-    await message.edit(f"<b>{all} сообщений будет удалено!</b>")
-    _ = ""
+async def delete(chat, message, now):
+    if now:
+        all = (await message.client.get_messages(chat, from_user="me")).total
+        await message.edit(f"<b>{all} сообщений будет удалено!</b>")
+    else: await message.delete()
+    _ = not now
     async for msg in message.client.iter_messages(chat, from_user="me"):
         if _:
             await msg.delete()
         else:
             _ = "_"
-    await message.delete()
+    await message.delete() if now else "хули мусара хули мусара хули, едем так как ехали даже в хуй не дули"
