2424ae2b0666ec6aa721657ff30433a5ca455546
KeyZenD <37895600+KeyZenD@users.noreply.github.com>
2022-04-15 20:28:05 +0800

    Update saved.py

--- saved.py
+++ saved.py
@@ -1,26 +1,24 @@
-import asyncio
-import logging
 import io
 from .. import loader, utils
 
-logger = logging.getLogger(__name__)
-
-
 @loader.tds
 class SavedMod(loader.Module):
-	"""Созранятель в избранное"""
-	strings = {"name": "SavedMessages"}
-
-	
+	"""Соxранятель в избранное"""
+	strings = {"name": "SavedMessages", "to":"me"}
 	@loader.unrestricted
 	async def savedcmd(self, message):
 		""".saved реплай на медиа"""
 		await message.delete()
 		reply = await message.get_reply_message()
+		name = utils.get_args_raw(message)
 		if not reply or not reply.file:
 			return
-		file = io.BytesIO()
-		file.name = reply.file.name or f"{reply.file.id}{reply.file.ext}"
-		await message.client.download_file(reply, file)
-		file.seek(0)
-		await message.client.send_file("me", file, force_document=True)
+		media = reply.media
+		if media.ttl_seconds: 
+			file = await reply.download_media(bytes)
+			file = io.BytesIO(file)
+			file.name = name or reply.sender_id + reply.file.ext
+			file.seek(0)
+			await message.client.send_file(self.strings["to"], file)
+		else:
+			await reply.forward_to(self.strings["to"])
